f980ed6745d715c0c13646c7e83ce06af061e67a
vietvuongle <vietvuongf@gmail.com>
2023-02-08 15:23:25 +0700

    Bai tap fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,32 @@
+package java_nhom12;
+
+public class fibonacci {
+
+	    public static void main(String[] args) {
+	        System.out.println("10 số đầu tiên của dãy số fibonacci: ");
+	        for (int i = 0; i < 10; i++) {
+	            System.out.print(fibonacci(i) + " ");
+	        }
+	    }
+	     
+	
+	    public static int fibonacci(int n) {
+	        int f0 = 0;
+	        int f1 = 1;
+	        int fn = 1;
+	 
+	        if (n < 0) {
+	            return -1;
+	        } else if (n == 0 || n == 1) {
+	            return n;
+	        } else {
+	            for (int i = 2; i < n; i++) {
+	                f0 = f1;
+	                f1 = fn;
+	                fn = f0 + f1;
+	            }
+	        }
+	        return fn;
+	}
+
+}
